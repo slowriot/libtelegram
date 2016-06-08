8a6a7ceaef886e3edc768f715421a478b2d27b7d
SlowRiot <slowriot@voxelstorm.com>
2016-06-08 20:16:55 +0100

    helper script to set a webhook

---
+++ set_webhook.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Simple script to tell telegram to set a webhook at a particular place
+
+token="$1"
+url="$2"
+if [ -z "$token" ]; then
+  echo "Usage: $0 token [url]" >&2
+  echo "Omit url argument to unset instead of setting." >&2
+  exit 1
+fi
+
+endpoint="https://api.telegram.org"
+api="$endpoint/bot$token"
+
+result=$(wget -q "$api/setWebhook?url=$url" -O -)
+# pretty-print the result only if jq is available
+jq_path=$(which jq)
+if [ -x "$jq_path" ]; then
+  jq . <<< "$result"
+else
+  echo "$result"
+fi
