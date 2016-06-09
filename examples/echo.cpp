a15eb93f79a787f53a385f19b1932426661cc1d7
SlowRiot <slowriot@voxelstorm.com>
2016-06-09 03:59:26 +0100

    removing extraneous debugging stuff from echo example

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -5,19 +5,10 @@ auto main()->int {
   std::string const token("229029247:AAHfNUvcGS_ttvvulZG3Qi4NmMrjebH8F6w");
   telegram::listener listener;
   telegram::sender sender(token);
-  listener.set_callback_raw([](std::string const &input){
-    std::cerr << "DEBUG: raw callback called with " << input << std::endl;
-  });
-  listener.set_callback_json([](boost::property_tree::ptree const &input){
-    std::cerr << "DEBUG: json callback called with update id " << input.get("update_id", "unknown") << std::endl;
-  });
   listener.set_callback_message([&](boost::property_tree::ptree const &input){
     int_fast64_t const message_chat_id(input.get<int_fast64_t>("chat.id", -1));
     std::string  const message_sender( input.get("from.first_name", "unknown"));
     std::string  const message_text(   input.get("text", ""));
-    std::cerr << "DEBUG: message callback called with message \"" << message_text << "\"" <<
-                 " from " << message_sender <<
-                 " in chat id " << message_chat_id << std::endl;
     sender.send_message(message_chat_id, message_sender + " sent \"" + message_text + "\" to chat id " + std::to_string(message_chat_id));
   });
   listener.run();
