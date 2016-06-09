05d22729ed4d3d2830e59101466108b3ec93676d
SlowRiot <slowriot@voxelstorm.com>
2016-06-10 00:46:46 +0100

    new, more minimal echo example using just native types

--- examples/echo.cpp
+++ examples/echo.cpp
@@ -6,11 +6,10 @@ auto main()->int {
   std::string const token("bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11");      // in practice you would probably read this from config file or database
   telegram::listener listener;                                                  // create a listener which will process incoming requests
   telegram::sender sender(token);                                               // create a sender with our token for outgoing messages
-  listener.set_callback_message([&](boost::property_tree::ptree const &input){  // we set a callback for receiving messages, using a lambda for convenience
-    std::string  const message_text(   input.get("text", ""));                  // message components are passed in boost property tree format
-    std::string  const message_sender( input.get("from.first_name", "unknown")); // we specify a default fallback in case the property isn't found
-    int_fast64_t const message_chat_id(input.get<int_fast64_t>("chat.id", -1)); // the return type can be deduced from the fallback's type, or specified manually
-    sender.send_message(message_chat_id, message_sender + " sent \"" + message_text + "\" to chat id " + std::to_string(message_chat_id)); // send our reply from within the callback
+  listener.set_callback_message([&](telegram::types::message const &message){   // we set a callback for receiving messages in native format, using a lambda for convenience
+    std::string const message_from(message.from ? message.from->first_name : "Unknown sender"); // some fields, such as message.from, are optional
+    std::string const reply(message_from + " sent \"" + message.text + "\" to chat id " + std::to_string(message.chat.id)); // each element is a native type - the chat id is an integer
+    sender.send_message(message.chat.id, reply);                                // send our reply from within the callback
   });
   listener.run();                                                               // launch the listener - this call blocks until the fastcgi server drops us
   return EXIT_SUCCESS;
