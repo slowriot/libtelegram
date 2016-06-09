2c3aa3241e0ee60ba1d1d510e4f1e1e9cc6991b4
SlowRiot <slowriot@voxelstorm.com>
2016-06-10 00:27:22 +0100

    implementing user type

--- include/libtelegram/types/user.h
+++ include/libtelegram/types/user.h
@@ -5,8 +5,28 @@ namespace telegram {
 namespace types {
 
 struct user {
-  // TODO
+  int_fast32_t id = 0;                                                          // Unique identifier for this user or bot
+  std::string first_name;                                                       // User‘s or bot’s first name
+  std::string last_name;                                                        // User‘s or bot’s last name
+  std::string username;                                                         // User‘s or bot’s username
+
+  static user const from_ptree(boost::property_tree::ptree const &tree);
+  static user const from_ptree(boost::property_tree::ptree const &tree, std::string const &path);
 };
+
+user const user::from_ptree(boost::property_tree::ptree const &tree) {
+  /// Factory to generate a struct of this type from the correct property tree
+  /// If any non-optional elements are missing from the tree, throws boost::property_tree::ptree_bad_path
+  return user{tree.get("id", 0),
+              tree.get("first_name", ""),
+              tree.get("last_name", ""),
+              tree.get("username", "")};
+}
+user const user::from_ptree(boost::property_tree::ptree const &tree, std::string const &path) {
+  /// Helper to generate a struct of this type from a path within a tree
+  /// If there is no such child, throws boost::property_tree::ptree_bad_path
+  return from_ptree(tree.get_child(path));
+}
 
 }
 }
