7513592c2b75eb58d5562212ee03d238611e389f
SlowRiot <slowriot@voxelstorm.com>
2016-09-01 15:05:28 +0100

    a user's last_name and username should both be optional fields

--- include/libtelegram/types/user.h
+++ include/libtelegram/types/user.h
@@ -7,8 +7,8 @@ namespace types {
 struct user {
   int_fast32_t id = 0;                                                          // Unique identifier for this user or bot
   std::string first_name;                                                       // User‘s or bot’s first name
-  std::string last_name;                                                        // User‘s or bot’s last name
-  std::string username;                                                         // User‘s or bot’s username
+  std::experimental::optional<std::string> last_name;                           // User‘s or bot’s last name
+  std::experimental::optional<std::string> username;                            // User‘s or bot’s username
 
   static user const from_json(nlohmann::json const &tree);
   static user const from_json(nlohmann::json const &tree, std::string const &path);
@@ -19,8 +19,8 @@ user const user::from_json(nlohmann::json const &tree) {
   /// If any non-optional elements are missing from the tree, throws std::domain_error
   return user{tree.at("id"),
               tree.at("first_name"),
-              tree.at("last_name"),
-              tree.at("username")};
+              make_optional<std::string>(tree, "last_name"),
+              make_optional<std::string>(tree, "username")};
 }
 user const user::from_json(nlohmann::json const &tree, std::string const &path) {
   /// Helper to generate a struct of this type from a path within a tree
