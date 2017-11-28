09e3207cd533bdc4149932d3a0a7e54cfade7b78
SlowRiot <slowriot@voxelstorm.com>
2017-11-28 23:58:51 +0000

    add game callback for inline keyboard buttons

---
+++ include/libtelegram/types/callback_game.h
@@ -0,0 +1,22 @@
+#ifndef TELEGRAM_TYPES_CALLBACK_GAME_H_INCLUDED
+#define TELEGRAM_TYPES_CALLBACK_GAME_H_INCLUDED
+
+#include "message.h"
+
+namespace telegram::types {
+
+struct callback_game {
+  /// See https://core.telegram.org/bots/api#callbackgame
+  // At the time of writing, currently a placeholder holding no information.
+
+  inline void get(nlohmann::json &tree) const;
+};
+
+inline void callback_game::get(nlohmann::json &tree [[maybe_unused]]) const {
+  /// Get this keyboard button into a json object (presuming the target is a key in a keyboard)
+  // noop
+}
+
+}
+
+#endif // TELEGRAM_TYPES_CALLBACK_GAME_H_INCLUDED
--- include/libtelegram/types/reply_markup/inline_keyboard_button.h
+++ include/libtelegram/types/reply_markup/inline_keyboard_button.h
@@ -2,7 +2,8 @@
 #define TELEGRAM_TYPES_REPLY_MARKUP_INLINE_KEYBOARD_BUTTON_H_INCLUDED
 
 #include <optional>
-//#include <experimental/variant>
+#include <variant>
+#include "libtelegram/types/callback_game.h"
 
 namespace telegram::types::reply_markup {
 
@@ -13,7 +14,7 @@ struct inline_keyboard_button {
     CALLBACK,
     SWITCH,
     SWITCH_CURRENT,
-    //GAME,
+    GAME,
     PAY
   };
 
@@ -21,38 +22,37 @@ struct inline_keyboard_button {
   std::optional<std::string> callback_data;                                     // Optional. Data to be sent in a callback query to the bot when button is pressed, 1-64 bytes.
   std::optional<std::string> switch_inline_query;                               // Optional. If set, pressing the button will prompt the user to select one of their chats, open that chat and insert the bot‘s username and the specified inline query in the input field. Can be empty, in which case just the bot’s username will be inserted.  Note: This offers an easy way for users to start using your bot in inline mode when they are currently in a private chat with it. Especially useful when combined with switch_pm… actions – in this case the user will be automatically returned to the chat they switched from, skipping the chat selection screen.
   std::optional<std::string> switch_inline_query_current_chat;                  // Optional. If set, pressing the button will insert the bot‘s username and the specified inline query in the current chat's input field. Can be empty, in which case only the bot’s username will be inserted.  This offers a quick way for the user to open your bot in inline mode in the same chat – good for selecting something from multiple options.
-  //std::optional<callback_game> callback_game;                                   // Optional. Description of the game that will be launched when the user presses the button.  NOTE: This type of button must always be the first button in the first row.
+  std::optional<types::callback_game> callback_game;                            // Optional. Description of the game that will be launched when the user presses the button.  NOTE: This type of button must always be the first button in the first row.
   bool pay = false;                                                             // Optional. Specify true, to send a Pay button.  NOTE: This type of button must always be the first button in the first row.
 
-  inline inline_keyboard_button(std::string const &this_text, type type, std::string const &content = {});
-  //inline inline_keyboard_button(std::string const &this_text, type type, std::variant<std::string, callback_game, bool> const &content = std::string{});
+  inline inline_keyboard_button(std::string const &this_text,
+                                type this_type,
+                                std::variant<std::string, types::callback_game, bool> const &content = std::string{});
 
   inline void get(nlohmann::json &tree) const;
 };
 
 inline inline_keyboard_button::inline_keyboard_button(std::string const &this_text,
                                                       type this_type,
-                                                      std::string const &content)
-                                                      //std::variant<std::string, bool> const &content)
+                                                      std::variant<std::string, types::callback_game, bool> const &content)
   : text(this_text) {
   /// Constructor
   switch(this_type) {
   case type::URL:
-    url = content;
+    url = std::get<std::string>(content);
     break;
   case type::CALLBACK:
-    callback_data = content;
+    callback_data = std::get<std::string>(content);
     break;
   case type::SWITCH:
-    switch_inline_query = content;
+    switch_inline_query = std::get<std::string>(content);
     break;
   case type::SWITCH_CURRENT:
-    switch_inline_query_current_chat = content;
+    switch_inline_query_current_chat = std::get<std::string>(content);
     break;
-  //case type::GAME:
-  //  callback_game = content;
-  //  break;
-  //
+  case type::GAME:
+    callback_game = std::get<types::callback_game>(content);
+    break;
   case type::PAY:
     pay = true;
     break;
@@ -74,9 +74,9 @@ inline void inline_keyboard_button::get(nlohmann::json &tree) const {
   if(switch_inline_query_current_chat) {
     tree["switch_inline_query_current_chat"] = *switch_inline_query_current_chat;
   }
-  //if(callback_game) {
-  //  tree["callback_game"] = *callback_game;
-  //}
+  if(callback_game) {
+    callback_game->get(tree["callback_game"]);
+  }
   if(pay) {
     tree["pay"] = true;
   }
