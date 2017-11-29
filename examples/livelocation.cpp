6cf75ccd33d16455ec1f9fb187f952d0630ab524
SlowRiot <slowriot@voxelstorm.com>
2017-11-29 01:44:34 +0000

    flip order of latitude and longitude in the struct, to make more sense

--- examples/livelocation.cpp
+++ examples/livelocation.cpp
@@ -1,5 +1,10 @@
 /// This example demonstrates a bot that accepts two locations, and then sends
 /// a live location interpolating smoothly between the two.
+/// As this is a very simple example, it does not keep track of multiple users,
+/// so should only be used by one user at a time.
+/// It could be extended to support multiple users by tracking each user's
+/// status along with user ID, with a separate thread running to update the live
+/// locations for each.
 
 #include <libtelegram/libtelegram.h>
 
@@ -8,9 +13,38 @@ auto main()->int {
   telegram::sender sender(token);
   telegram::listener::poll listener(sender);
 
-  listener.set_callback_message([&](telegram::types::message const &message){
+  std::optional<telegram::types::location> first_location;                      // cache our first location, when it arrives
+  std::optional<telegram::types::location> second_location;                     // cache our second location, when it arrives
 
+  listener.set_callback_message([&](telegram::types::message const &message){
+    auto message_chat_id = message.chat.id;
+    if(!first_location) {                                                       // we haven't received any locations yet from the user
+      if(!message.location) {                                                   // early exit if this message doesn't contain a location
+        sender.send_message(message_chat_id, "Send me the first location.");
+        return;
+      }
+      first_location = message.location;                                        // cache the first location
+      sender.send_message(message_chat_id,
+                          "Interpolating from " +
+                          std::to_string(first_location->longitude) + ", " +
+                          std::to_string(first_location->latitude) + ".\n"
+                          "Send me the second location.");
+    } else {                                                                    // we already received the first location, so we want the second
+      if(!message.location) {                                                   // early exit if this message doesn't contain a location
+        sender.send_message(message_chat_id, "Send me the second location.");
+        return;
+      }
+      second_location = message.location;                                       // cache the second location
+      sender.send_message(message_chat_id,
+                          "Interpolating from " +
+                          std::to_string(first_location->longitude) + ", " +
+                          std::to_string(first_location->latitude) + " to " +
+                          std::to_string(second_location->longitude) + ", " +
+                          std::to_string(second_location->latitude) + "...\n");
+      // TODO: loop here
+    }
   });
+  listener.set_num_threads(1);                                                  // run single-threaded
   listener.run();                                                               // launch the listener - this call blocks until interrupted
   return EXIT_SUCCESS;
 };
--- include/libtelegram/types/location.h
+++ include/libtelegram/types/location.h
@@ -5,8 +5,8 @@ namespace telegram::types {
 
 struct location {
   /// See https://core.telegram.org/bots/api#location
-  float longitude = 0.0f;                                                       // Longitude as defined by sender
   float latitude  = 0.0f;                                                       // Latitude as defined by sender
+  float longitude = 0.0f;                                                       // Longitude as defined by sender
 
   static location const from_json(nlohmann::json const &tree);
   static location const from_json(nlohmann::json const &tree, std::string const &path);
@@ -15,8 +15,8 @@ struct location {
 inline location const location::from_json(nlohmann::json const &tree) {
   /// Factory to generate a struct of this type from the correct property tree
   /// If any non-optional elements are missing from the tree, throws std::domain_error
-  return location{tree.at("longitude"),
-                  tree.at("latitude")};
+  return location{tree.at("latitude"),
+                  tree.at("longitude")};
 }
 inline location const location::from_json(nlohmann::json const &tree, std::string const &path) {
   /// Helper to generate a struct of this type from a path within a tree
